7ee94912e230ce54f20a692a88099ed33b8092e1
Greg McGrath <greg@mcgrathg.com>
2016-10-13 16:20:06 -0400

    social button tooltips and styling

--- app/components/SocialBar/index.js
+++ app/components/SocialBar/index.js
@@ -19,11 +19,11 @@ function SocialBar({ ...props }) {
       {...rest}
     >
       <ul className="text-center list-inline">
-        <SocialButton href="https://www.facebook.com/greg.mcgrath" iconClassName="fa fa-facebook" />
-        <SocialButton href="https://github.com/mcgrathg" iconClassName="fa fa-github" />
-        <SocialButton href="http://stackoverflow.com/users/3688026/mcgrathg" iconClassName="fa fa-stack-overflow" />
-        <SocialButton href="https://www.linkedin.com/in/gregmcgrath" iconClassName="fa fa-linkedin-square" />
-        <SocialButton href="https://plus.google.com/+GregMcGrath8" iconClassName="fa fa-google-plus" />
+        <SocialButton href="https://github.com/mcgrathg" className={styles.socialButton} iconClassName="fa fa-github" name="Github" />
+        <SocialButton href="http://stackoverflow.com/users/3688026/mcgrathg" className={styles.socialButton} iconClassName="fa fa-stack-overflow" name="Stack Overflow" />
+        <SocialButton href="https://www.linkedin.com/in/gregmcgrath" className={styles.socialButton} iconClassName="fa fa-linkedin-square" name="LinkedIn" />
+        <SocialButton href="https://www.facebook.com/greg.mcgrath" className={styles.socialButton} iconClassName="fa fa-facebook" name="Facebook" />
+        <SocialButton href="https://plus.google.com/+GregMcGrath8" className={styles.socialButton} iconClassName="fa fa-google-plus" name="Google" />
       </ul>
     </div>
   );
--- app/components/SocialButton/index.js
+++ app/components/SocialButton/index.js
@@ -8,14 +8,14 @@ import React, { PropTypes } from 'react';
 import styles from './styles.css';
 
 function SocialButton({ ...props }) {
-  const { iconClassName, href, ...rest } = props;
+  const { iconClassName, href, name, ...rest } = props;
   return (
     <li
       className={styles.socialButton}
       {...rest}
     >
-      <a href={href} className={styles.socialLink} target="_blank">
-        <i className={iconClassName} aria-hidden="true"></i>
+      <a href={href} className={styles.socialLink} target="_blank" title="link">
+        <i className={iconClassName} aria-hidden="true" title={name}></i>
       </a>
     </li>
   );
@@ -24,7 +24,7 @@ function SocialButton({ ...props }) {
 SocialButton.propTypes = {
   iconClassName: PropTypes.string.isRequired,
   href: PropTypes.string.isRequired,
-
+  name: PropTypes.string,
 };
 
 export default SocialButton;
