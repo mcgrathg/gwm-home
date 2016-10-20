070f99896bdab18e3c3514d9cf367de2f9faf7ab
Greg McGrath <greg@mcgrathg.com>
2016-10-20 16:40:07 -0400

    moved Navigationt to its own Component

---
+++ app/components/Navigation/index.js
@@ -0,0 +1,48 @@
+/**
+*
+* Navigation
+*
+*/
+
+import React from 'react';
+
+import { Image, Grid, Row, Col, Nav, NavItem } from 'react-bootstrap';
+import { IndexLinkContainer, LinkContainer } from 'react-router-bootstrap';
+
+import Logo from './logo.png';
+
+import styles from './styles.css';
+
+function Navigation() {
+  return (
+    <Grid className={styles.navigation}>
+      <Row className={styles.navigationRow}>
+        <Col xs={4} sm={2} md={2} className={styles.logoWrapper}>
+          <IndexLinkContainer to="/">
+            <NavItem>
+              <Image className={styles.logo} src={Logo} alt="Greg McGrath -  Logo" />
+            </NavItem>
+          </IndexLinkContainer>
+        </Col>
+        <Col xs={8} sm={10} md={10}>
+          <Nav pullRight justified bsStyle="tabs">
+            <IndexLinkContainer to="/">
+              <NavItem eventKey={1}>Home</NavItem>
+            </IndexLinkContainer>
+            <LinkContainer to="/resume">
+              <NavItem eventKey={2}>Resume</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/portfolio">
+              <NavItem eventKey={2}>Portfolio</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/contact">
+              <NavItem eventKey={2}>Contact</NavItem>
+            </LinkContainer>
+          </Nav>
+        </Col>
+      </Row>
+    </Grid>
+  );
+}
+
+export default Navigation;
--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -15,11 +15,8 @@ import 'sanitize.css/sanitize.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.css';
 
+import Navigation from 'components/Navigation';
 import Footer from 'components/Footer';
-import Logo from './logo.png';
-
-import { Image, Grid, Row, Col, Nav, NavItem } from 'react-bootstrap';
-import { IndexLinkContainer, LinkContainer } from 'react-router-bootstrap';
 
 import styles from './styles.css';
 
@@ -33,33 +30,7 @@ function App(props) {
           { name: 'description', content: 'Greg McGrath\'s Site' },
         ]}
       />
-      <Grid className={styles.navigation}>
-        <Row className={styles.navigationRow}>
-          <Col xs={4} sm={2} md={2} className={styles.logoWrapper}>
-            <IndexLinkContainer to="/">
-              <NavItem>
-                <Image className={styles.logo} src={Logo} alt="Greg McGrath -  Logo" />
-              </NavItem>
-            </IndexLinkContainer>
-          </Col>
-          <Col xs={8} sm={10} md={10}>
-            <Nav pullRight justified bsStyle="tabs">
-              <IndexLinkContainer to="/">
-                <NavItem eventKey={1}>Home</NavItem>
-              </IndexLinkContainer>
-              <LinkContainer to="/resume">
-                <NavItem eventKey={2}>Resume</NavItem>
-              </LinkContainer>
-              <LinkContainer to="/portfolio">
-                <NavItem eventKey={2}>Portfolio</NavItem>
-              </LinkContainer>
-              <LinkContainer to="/contact">
-                <NavItem eventKey={2}>Contact</NavItem>
-              </LinkContainer>
-            </Nav>
-          </Col>
-        </Row>
-      </Grid>
+      <Navigation />
       {React.Children.toArray(props.children)}
       <Footer />
     </div>
