2f2f48b97aad3ee8d41d7fa9f8b9b3569d100a4f
Greg McGrath <greg@mcgrathg.com>
2016-10-22 12:57:51 -0400

    added gray color; fixed tEvent styling

--- app/components/TimelineEvent/index.js
+++ app/components/TimelineEvent/index.js
@@ -80,7 +80,9 @@ class TimelineEvent extends Component { // eslint-disable-line react/prefer-stat
             </span>);
           }
           return (
-            <ListGroupItem key={idx} className={styles.responsibility}>{val}</ListGroupItem>
+            <ListGroupItem key={idx} className={styles.responsibility}>
+              <p className={styles.responsibilityText}>{val}</p>
+            </ListGroupItem>
           );
         })}
       </ListGroup>
