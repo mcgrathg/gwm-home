608489ce9aa8fa3ddc38d2bf685dc482b93f86ae
Greg McGrath <greg@mcgrathg.com>
2016-10-24 14:21:48 -0400

    simplified map

--- app/components/Map/index.js
+++ app/components/Map/index.js
@@ -15,34 +15,16 @@ import fancyMapStyles from './fancyMapStyles.json';
 
 import styles from './styles.css';
 
-function createMapOptions(maps) {
-  // next props are exposed at maps
-  // "Animation", "ControlPosition", "MapTypeControlStyle", "MapTypeId",
-  // "NavigationControlStyle", "ScaleControlStyle", "StrokePosition", "SymbolPath", "ZoomControlStyle",
-  // "DirectionsStatus", "DirectionsTravelMode", "DirectionsUnitSystem", "DistanceMatrixStatus",
-  // "DistanceMatrixElementStatus", "ElevationStatus", "GeocoderLocationType", "GeocoderStatus", "KmlLayerStatus",
-  // "MaxZoomStatus", "StreetViewStatus", "TransitMode", "TransitRoutePreference", "TravelMode", "UnitSystem"
-  return {
-    zoomControlOptions: {
-      position: maps.ControlPosition.RIGHT_CENTER,
-      style: maps.ZoomControlStyle.SMALL,
-    },
-    mapTypeControlOptions: {
-      position: maps.ControlPosition.TOP_RIGHT,
-    },
-    mapTypeControl: true,
-    styles: fancyMapStyles,
-  };
-}
-
-function Map({ center, zoom }) {
+function Map({ center, zoom, mapStyle = fancyMapStyles }) {
   return (
     <GoogleMap
+      bootstrapURLKeys={{
+        key: GOOGLE_API_KEY,
+      }}
       className={styles.map}
-      apiKey={GOOGLE_API_KEY}
       center={center}
       zoom={zoom}
-      options={createMapOptions}
+      options={{ styles: mapStyle }}
     />
   );
 }
@@ -50,12 +32,12 @@ function Map({ center, zoom }) {
 Map.propTypes = {
   center: PropTypes.array,
   zoom: PropTypes.number,
+  mapStyle: PropTypes.any,
 };
 
 Map.defaultProps = {
-  center: [42.998124, -73.8437357],
+  center: [42.9976638, -73.8899404],
   zoom: 9,
 };
-
 
 export default Map;
