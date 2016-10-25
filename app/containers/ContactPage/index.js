e08a0893869be019ff8ca8c7a1f108e0ef112d32
Greg McGrath <greg@mcgrathg.com>
2016-10-25 10:30:44 -0400

    added toast & masking

--- app/containers/ContactPage/index.js
+++ app/containers/ContactPage/index.js
@@ -14,33 +14,83 @@ import { ParentContextMixin, Input, Textarea } from 'formsy-react-components';
 import { Button } from 'react-bootstrap';
 
 import {
+  ToastContainer,
+  ToastMessage,
+} from 'react-toastr';
+
+import {
   selectWindowWidth,
 } from 'containers/App/selectors';
 
 import Map from 'components/Map';
 import H2 from 'components/H2';
 import HeaderIcon from 'components/HeaderIcon';
+import LoadingIndicator from 'components/LoadingIndicator';
 
 import btnStyle from './buttons.css';
 import styles from './styles.css';
 
 const MIN_WIN_WIDTH = 767;
+const ToastMessageFactory = React.createFactory(ToastMessage.animation);
+
 
 export class ContactPage extends Component { // eslint-disable-line react/prefer-stateless-function
 
   mixins: [ParentContextMixin]
 
+  constructor(props) {
+    super(props);
+
+    this.addAlert = this.addAlert.bind(this);
+    this.clearAlert = this.clearAlert.bind(this);
+    this.enableButton = this.enableButton.bind(this);
+    this.disableButton = this.disableButton.bind(this);
+    this.submitForm = this.submitForm.bind(this);
+
+    this.state = {
+      canSubmit: false,
+      isSubmitting: false,
+    };
+  }
+
+  addAlert(msg) {
+    this.container.success(`${msg}`, 'Message Sent', {
+      closeButton: true,
+    });
+  }
+
+  clearAlert() {
+    this.container.clear();
+  }
+
   submitForm = (data) => {
     console.log(data);
+    this.addAlert(JSON.stringify(data, null, 4));
+    this.contactForm.reset();
+    this.contactForm.disable();
+  }
+
+  enableButton() {
+    this.setState({ canSubmit: true });
+  }
+
+  disableButton() {
+    this.setState({ canSubmit: false });
   }
 
   render() {
     const { windowWidth } = this.props;
+    const { isSubmitting } = this.state;
     const mapProps = {};
 
     if (windowWidth < MIN_WIN_WIDTH) {
       // window is too small to see town, move to right
       mapProps.center = [42.999045, -73.850716];
+    }
+
+    let submittingIndicator;
+    if (isSubmitting) {
+      submittingIndicator = (<div className={styles.loadingIndicator}><LoadingIndicator /></div>);
     }
 
     return (
@@ -51,6 +101,11 @@ export class ContactPage extends Component { // eslint-disable-line react/prefer
             { name: 'description', content: 'Description of ContactPage' },
           ]}
         />
+        <ToastContainer
+          toastMessageFactory={ToastMessageFactory}
+          ref={(c) => (this.container = c)}
+          className="toast-top-right"
+        />
         <div className="container">
           <div className={styles.map} >
             <Map
@@ -58,15 +113,17 @@ export class ContactPage extends Component { // eslint-disable-line react/prefer
               {...mapProps}
             />
             <Form
+              disabled={isSubmitting}
               className={styles.contactForm}
               onSubmit={this.submitForm}
-              ref={(c) => { this.contactForm = c; }}
+              onValid={this.enableButton}
+              onInvalid={this.disableButton}
+              ref={(c) => (this.contactForm = c)}
             >
               <H2>
                 <HeaderIcon className="fa-paper-plane" />
                 Send a Message
               </H2>
-
               <Input
                 name="name"
                 value=""
@@ -106,13 +163,20 @@ export class ContactPage extends Component { // eslint-disable-line react/prefer
                 type="text"
                 required
               />
-              <Button className={classNames('pull-right', btnStyle.submitBtn)} formNoValidate type="submit" defaultValue="Submit" >Send</Button>
+              <Button
+                className={classNames('pull-right', btnStyle.submitBtn)}
+                type="submit"
+                disabled={!this.state.canSubmit}
+              >
+                Send
+              </Button>
+              {submittingIndicator}
             </Form>
           </div>
           <h2>test</h2>
         </div>
       </div>
-        );
+    );
   }
 }
 
