e9774255eef81ddbee21c91233491f4834a8da30
Greg McGrath <greg@mcgrathg.com>
2016-10-26 16:05:29 -0400

    linted

--- app/containers/ContactForm/tests/selectors.test.js
+++ app/containers/ContactForm/tests/selectors.test.js
@@ -1,11 +1,11 @@
-// import { selectContactFormDomain } from '../selectors';
-// import { fromJS } from 'immutable';
-import expect from 'expect';
-
-// const selector = selectContactFormDomain();
-
-describe('selectContactFormDomain', () => {
-  it('Expect to have unit tests specified', () => {
-    expect('Test case').toEqual(false);
-  });
-});
+// // import { selectContactFormDomain } from '../selectors';
+// // import { fromJS } from 'immutable';
+// import expect from 'expect';
+//
+// // const selector = selectContactFormDomain();
+//
+// describe('selectContactFormDomain', () => {
+//   it('Expect to have unit tests specified', () => {
+//     expect('Test case').toEqual(false);
+//   });
+// });
