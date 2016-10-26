644043b63392ef1e5d1c3a9e13899ef71a973e52
Greg McGrath <greg@mcgrathg.com>
2016-10-25 22:33:39 -0400

    message sending actions (start, success, failure)

--- app/containers/ContactForm/actions.js
+++ app/containers/ContactForm/actions.js
@@ -1,15 +1,51 @@
-// /*
-//  *
-//  * ContactForm actions
-//  *
-//  */
-//
-// import {
-//   DEFAULT_ACTION,
-// } from './constants';
-//
-// export function defaultAction() {
-//   return {
-//     type: DEFAULT_ACTION,
-//   };
-// }
+/*
+ *
+ * ContactForm actions
+ *
+ */
+
+import {
+  SEND_MESSAGE,
+  SEND_MESSAGE_SUCCESS,
+  SEND_MESSAGE_ERROR,
+} from './constants';
+
+/**
+ * Send email based on Contact Form, this action starts the POST saga
+ *
+ * @return {object} An action object with a type of LOAD_VIDEOS
+ */
+export function sendMessage(data) {
+  return {
+    type: SEND_MESSAGE,
+    data,
+  };
+}
+
+/**
+ * Dispatched when the contact email was sent successfully by the POST saga
+ *
+ * @param  {array} email Contents of email sent
+ *
+ * @return {object} An action object with a type of SEND_MESSAGE_SUCCESS passing the message details
+ */
+export function messageSent({ email }) {
+  return {
+    type: SEND_MESSAGE_SUCCESS,
+    email,
+  };
+}
+
+/**
+ * Dispatched when sending contact email fails
+ *
+ * @param  {object} error The error
+ *
+ * @return {object} An action object with a type of SEND_MESSAGE_ERROR passing the error
+ */
+export function messageSentError(error) {
+  return {
+    type: SEND_MESSAGE_ERROR,
+    error,
+  };
+}
