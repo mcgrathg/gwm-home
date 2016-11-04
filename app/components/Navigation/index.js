1d4f16ed2a6973e8385a0955a45386d10fe7fcec
Greg McGrath <greg@mcgrathg.com>
2016-11-04 17:00:25 -0400

    beginning to build using row/col, not nav

--- app/components/Navigation/index.js
+++ app/components/Navigation/index.js
@@ -6,43 +6,51 @@
 
 import React, { PropTypes } from 'react';
 import classNames from 'classnames';
-import { Image, Grid, Row, Col, Nav, NavItem } from 'react-bootstrap';
-import { IndexLinkContainer, LinkContainer } from 'react-router-bootstrap';
+import { Button, Image, Grid, Row, Col, Nav, NavItem } from 'react-bootstrap';
+// import { IndexLinkContainer, LinkContainer } from 'react-router-bootstrap';
+//
+import { Link, IndexLink } from 'react-router';
 
-import Logo from './gmg6.png';
+
+import Logo from './gmg-brackets.png';
+// import Logo from './gmg-dbl-braces.png';
 
 import styles from './styles.css';
 
 function Navigation({ currentRoute }) {
   const currentPath = currentRoute.path;
   return (
-    <Grid className={styles.navigation}>
-      <Row className={classNames('container', styles.navigationRow)}>
-        <Col xs={3} sm={4} md={3} className={styles.logoWrapper}>
-          <IndexLinkContainer to="/">
-            <NavItem>
-              <Image responsive className={styles.logo} src={Logo} alt="Greg McGrath -  Logo" />
-            </NavItem>
-          </IndexLinkContainer>
-        </Col>
-        <Col xs={4} sm={8} md={9} className={styles.navlinksCol}>
-          <Nav pullRight justified bsStyle="tabs">
-            <IndexLinkContainer to="/" active={currentPath === '/'}>
-              <NavItem>Home</NavItem>
-            </IndexLinkContainer>
-            <LinkContainer to="/resume" active={currentPath === '/resume'}>
-              <NavItem>Resume</NavItem>
-            </LinkContainer>
-            <LinkContainer to="/portfolio" active={currentPath === '/portfolio'}>
-              <NavItem>Portfolio</NavItem>
-            </LinkContainer>
-            <LinkContainer to="/contact" active={currentPath === '/contact'}>
-              <NavItem>Contact</NavItem>
-            </LinkContainer>
-          </Nav>
-        </Col>
-      </Row>
-    </Grid>
+    <div className={classNames(styles.navigation)}>
+      <div className="container">
+        <div className={classNames(styles.fixedCol, 'pull-left')}>
+          <IndexLink to="/">
+            <Image responsive className={styles.logo} src={Logo} alt="Greg McGrath -  Logo" />
+          </IndexLink>
+        </div>
+        <Row className={classNames(styles.fluidCol)}>
+          <Col sm={3}>
+            <IndexLink to="/" >
+              Home
+            </IndexLink>
+          </Col>
+          <Col sm={3}>
+            <Link to="/resume" >
+              Resume
+            </Link>
+          </Col>
+          <Col sm={3}>
+            <Link to="/portfolio" >
+              Portfolio
+            </Link>
+          </Col>
+          <Col sm={3}>
+            <Link to="/contact" >
+              Contact
+            </Link>
+          </Col>
+        </Row>
+      </div>
+    </div>
   );
 }
 
