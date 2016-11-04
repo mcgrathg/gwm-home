ea6081f99ac5d7269e4e7fa92837c16fbc6e731c
Greg McGrath <greg@mcgrathg.com>
2016-11-04 16:44:26 -0400

    added header css class

--- app/components/H3/index.js
+++ app/components/H3/index.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import classNames from 'classnames';
 
-function H3(props) {
+function H3({ className, ...rest }) {
   return (
-    <h3 {...props} />
+    <h3 className={classNames(className, 'header')} {...rest} />
   );
 }
+
+H3.propTypes = {
+  className: PropTypes.string,
+};
 
 export default H3;
