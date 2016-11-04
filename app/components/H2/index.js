e2d1afd863d7e9bdf222d205cde24031af3a7e77
Greg McGrath <greg@mcgrathg.com>
2016-11-04 16:43:31 -0400

    toggle option for bottom border

--- app/components/H2/index.js
+++ app/components/H2/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
+import classNames from 'classnames';
 import styles from './styles.css';
 
-function H2(props) {
+function H2({ className, disableBorder = false, ...rest }) {
   return (
-    <h2 className={styles.heading2} {...props} />
+    <h2 className={classNames(styles.heading2, className, 'header', { [styles.hasBorder]: !disableBorder })} {...rest} />
   );
 }
+
+H2.propTypes = {
+  className: PropTypes.string,
+  disableBorder: PropTypes.bool, // option to remove bottom border styling
+};
 
 export default H2;
