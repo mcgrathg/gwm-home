b9aa5ba0d9b9f4c4d41be118ff548cec9e56af70
Greg McGrath <greg@mcgrathg.com>
2016-11-04 16:58:36 -0400

    added contacts and image

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -8,7 +8,9 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import Helmet from 'react-helmet';
 import classNames from 'classnames';
-import { Image, Grid, Row, Col } from 'react-bootstrap';
+import { Image, Grid, Row, Col, Button } from 'react-bootstrap';
+
+import { Link } from 'react-router';
 
 import H1 from 'components/H1';
 import Profile from './profile.jpg';
@@ -33,11 +35,14 @@ export class HomePage extends Component { // eslint-disable-line react/prefer-st
                 <H1>
                   Hello!
                 </H1>
-                <Row>
-                  <Col sm={2}>
-                    <Image responsive circle className={styles.logo} src={Profile} alt="Greg McGrath" />
+                <Row className={styles.rowEqualHeight}>
+                  <Col sm={3} className={styles.leftCol}>
+                    <div className={styles.centeredBlock}>
+                      <Image responsive circle className={styles.profile} src={Profile} alt="Greg McGrath" />
+                      <SocialBar className={styles.social} liClassName={styles.socialListItem} />
+                    </div>
                   </Col>
-                  <Col sm={10}>
+                  <Col sm={9}>
                     <p className={classNames('lead', styles.lead)}>
                       Hello! I&apos;m Greg McGrath and I am a software developer with over 9 years of professional experience specializing in the front end and web applications.
                     </p>
@@ -53,10 +58,12 @@ export class HomePage extends Component { // eslint-disable-line react/prefer-st
                     <p className={classNames('lead', styles.lead)}>
                       If you’d like to get in touch, feel free to say hello through any of the social links below or messaging me through my website.
                     </p>
+                    <div className="text-center">
+                      <Link to="/contact"><Button className={styles.circle}>Contact Me</Button></Link>
+                    </div>
                   </Col>
                 </Row>
               </Grid>
-              <SocialBar className={styles.social} liClassName={styles.socialListItem} />
             </section>
           </div>
         </article>
