0aff7b86decef99b5763670bda0fc69ce8f89e7c
Greg McGrath <greg@mcgrathg.com>
2016-11-07 10:41:04 -0500

    moving all links to file

---
+++ app/components/Navigation/links.js
@@ -0,0 +1,22 @@
+/*
+ * HomePage text
+ *
+ * This contains all text for the major sections of the resume.
+ */
+
+export const links = [
+  {
+    text: 'Home',
+    to: '/',
+    isIndex: true,
+  }, {
+    text: 'Resume',
+    to: '/resume',
+  }, {
+    text: 'Portfolio',
+    to: '/portfolio',
+  }, {
+    text: 'Contact',
+    to: '/contact',
+  },
+];
