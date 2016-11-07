080395cdf9c32cd88e890ee8723d0f7eef65df41
Greg McGrath <greg@mcgrathg.com>
2016-11-07 08:48:13 -0500

    import intro text

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -16,6 +16,8 @@ import H1 from 'components/H1';
 import Profile from './profile.jpg';
 import SocialBar from 'components/SocialBar';
 
+import { intro } from './text';
+
 import styles from './styles.css';
 
 export class HomePage extends Component { // eslint-disable-line react/prefer-stateless-function
@@ -32,30 +34,16 @@ export class HomePage extends Component { // eslint-disable-line react/prefer-st
           <div>
             <section className={classNames(styles.textSection, styles.centered)}>
               <Grid className={styles.intro}>
-                <H1>
-                  Hello!
-                </H1>
                 <Row className={styles.rowEqualHeight}>
                   <Col sm={3} className={styles.leftCol}>
                     <Image responsive circle className={styles.profile} src={Profile} alt="Greg McGrath" />
                     <SocialBar className={styles.social} liClassName={styles.socialListItem} />
                   </Col>
                   <Col sm={9}>
-                    <p className={classNames('lead', styles.lead)}>
-                      Hello! I&apos;m Greg McGrath and I am a software developer with over 9 years of professional experience specializing in the front end and web applications.
-                    </p>
-                    <p className={classNames('lead', styles.lead)}>
-                      I&apos;m adept at problem solving, have a passion for details, can handle ambitious deadlines, and am a strong team player.
-                    </p>
-                    <p className={classNames('lead', styles.lead)}>
-                      I love reading to my son, playing fetch with my Jack Russells, and supporting the Chicago Cubs and AC Milan.
-                    </p>
-                    <p className={classNames('lead', styles.lead)}>
-                      I live in Ballston Spa in Upstate New York.
-                    </p>
-                    <p className={classNames('lead', styles.lead)}>
-                      If you’d like to get in touch, feel free to say hello through any of the social links below or messaging me through my website.
-                    </p>
+                    <H1 className="header">
+                      Hello!
+                    </H1>
+                    { intro.map((txt, idx) => <p key={idx} className={classNames('lead', styles.lead)}>{txt}</p>) }
                   </Col>
                 </Row>
                 <div className="text-center">
