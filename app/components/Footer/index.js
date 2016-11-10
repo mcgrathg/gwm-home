cf2f5549e32d18962ed429e36de990d6ecc56886
Greg McGrath <greg@mcgrathg.com>
2016-11-10 11:37:12 -0500

    beginning of footer

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -1,24 +1,35 @@
 import React from 'react';
 import classNames from 'classnames';
+import { Grid, Row, Col } from 'react-bootstrap';
+
+import SocialBar from 'components/SocialBar';
+
+import { links } from 'components/Navigation/links';
 
 import styles from './styles.css';
 
 function Footer() {
   return (
     <footer className={classNames(styles.footer)}>
-      <div className={classNames(styles.columns, 'container')}>
-        <section>
-          <p>
-            All Links
-          </p>
-        </section>
-        <section>
-          Made By Greg McGrath
-        </section>
-        <section>
-          Git Repo Link
-        </section>
-      </div>
+      <Grid>
+        <Row>
+          <Col md={4} className="text-left">Page Links</Col>
+          <Col md={4} className="text-center">
+            <SocialBar className={styles.social} iconClassName={styles.socialButton} />
+            Made By Greg McGrath
+          </Col>
+          <Col md={4} className="text-right">
+            <a
+              className={styles.btnLink}
+              href="https://github.com/mcgrathg/sample-extjs"
+              target="_blank"
+            >
+              Site&apos;s Git Repo
+              <i className={classNames('fa fa-github-alt', styles.icon)} />
+            </a>
+          </Col>
+        </Row>
+      </Grid>
     </footer>
   );
 }
