1105bbe61d29e99a532289a34f3e557b92e2b5ad
Greg McGrath <greg@mcgrathg.com>
2016-11-11 10:27:22 -0500

    added FooterNav

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -3,8 +3,7 @@ import classNames from 'classnames';
 import { Grid, Row, Col } from 'react-bootstrap';
 
 import SocialBar from 'components/SocialBar';
-
-import { links } from 'components/Navigation/links';
+import FooterNavigation from 'components/FooterNavigation';
 
 import styles from './styles.css';
 
@@ -13,7 +12,9 @@ function Footer() {
     <footer className={classNames(styles.footer)}>
       <Grid>
         <Row>
-          <Col md={4} className="text-left">Page Links</Col>
+          <Col md={4} className="text-left">
+            <FooterNavigation />
+          </Col>
           <Col md={4} className="text-center">
             <SocialBar className={styles.social} iconClassName={styles.socialButton} />
             Made By Greg McGrath
