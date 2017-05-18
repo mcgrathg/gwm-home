88389f74675242afe2658e3ca16f754ba8542afd
Greg McGrath <mcgrathg@users.noreply.github.com>
2017-05-18 13:44:20 -0400

    fixed dev/prod paths for public static files

--- server/middlewares/frontendMiddleware.js
+++ server/middlewares/frontendMiddleware.js
@@ -62,17 +62,22 @@ const addProdMiddlewares = (app, options) => {
 module.exports = (app, options) => {
   const isProd = process.env.NODE_ENV === 'production';
 
-  // serves up static files from the public folder.  All `/public` files will be served up
-  // as the file it is
-  app.use(express.static(path.resolve(process.cwd(), 'public')));
-
+  let publicDir;
   if (isProd) {
     addProdMiddlewares(app, options);
+    publicDir = 'public';
   } else {
     const webpackConfig = require('../../internals/webpack/webpack.dev.babel');
     addDevMiddlewares(app, webpackConfig);
+    publicDir = './app/public';
   }
 
+  console.log(publicDir);
+  console.log(path.resolve(process.cwd(), publicDir));
+
+  // serves up static files from the public folder.  All `/public` files will be served up
+  // as the file it is
+  app.use(express.static(path.resolve(process.cwd(), publicDir)));
 
   return app;
 };
