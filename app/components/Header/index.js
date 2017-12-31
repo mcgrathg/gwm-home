27a605c84abc26e0b63d5b5fc79c83892e168e62
Greg McGrath <greg@mcgrathg.com>
2017-12-31 16:03:33 -0500

    converted header to flexbox

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,35 +1,27 @@
 /**
-* Header
-*
-*/
+ * Header
+ *
+ */
 
 import React from 'react';
-import classNames from 'classnames';
-import { Image, Grid, Row, Col } from 'react-bootstrap';
+import { Image } from 'react-bootstrap';
 import { IndexLink } from 'react-router';
-
 import Navigation from 'containers/Navigation';
 import Logo from './logo.png';
-
 import styles from './styles.css';
 
-function Header() {
-  return (
-    <div className={classNames(styles.header, 'header')}>
-      <Grid>
-        <Row>
-          <Col sm={5} md={4}>
-            <IndexLink to="/">
-              <Image responsive className={styles.logo} src={Logo} alt="Greg McGrath -  Logo" />
-            </IndexLink>
-          </Col>
-          <Col sm={7} md={8}>
-            <Navigation navGroupClass={styles.navGroup} />
-          </Col>
-        </Row>
-      </Grid>
+const Header = () => (
+  <div className={styles.header}>
+    <div className={styles.wrapper}>
+      <IndexLink to="/" className={styles.logo}>
+        <Image responsive src={Logo} alt="Greg McGrath -  Logo" />
+      </IndexLink>
+      <Navigation
+        className={styles.navigation}
+        navGroupClass={styles.navGroup}
+      />
     </div>
-  );
-}
+  </div>
+);
 
 export default Header;
