30b62fb2ce949f9d9773690743288fcb21ce04db
Greg McGrath <greg@mcgrathg.com>
2018-01-07 22:49:23 -0500

    using fix width FA icons

--- app/components/SocialBar/social-networks.js
+++ app/components/SocialBar/social-networks.js
@@ -8,26 +8,26 @@ export const sites = [
   {
     name: 'Github',
     href: 'https://github.com/mcgrathg',
-    icon: 'fab fa-github',
+    icon: 'fab fa-github fa-fw',
   },
   {
     name: 'Stack Overflow',
     href: 'https://stackoverflow.com/users/3688026/mcgrathg',
-    icon: 'fab fa-stack-overflow',
+    icon: 'fab fa-stack-overflow fa-fw',
   },
   {
     name: 'LinkedIn',
     href: 'https://www.linkedin.com/in/gregmcgrath',
-    icon: 'fab fa-linkedin',
+    icon: 'fab fa-linkedin fa-fw',
   },
   {
     name: 'Facebook',
     href: 'https://www.facebook.com/greg.mcgrath',
-    icon: 'fab fa-facebook-f',
+    icon: 'fab fa-facebook-f fa-fw',
   },
   {
     name: 'Google',
     href: 'https://plus.google.com/+GregMcGrath8',
-    icon: 'fab fa-google-plus-g',
+    icon: 'fab fa-google-plus-g fa-fw',
   },
 ];
