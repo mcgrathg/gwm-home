334c46f95027c5216241a065646a5f41d82e8d52
Greg McGrath <Greg.McGrath@contractor.cengage.com>
2018-01-18 14:43:09 -0500

    fixed footer styling

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -9,27 +9,28 @@ import textStyle from '../../responsive-text-formatting.css';
 import styles from './styles.css';
 
 function Footer() {
-  const colStyle = (def) => classNames(textStyle['text-center-sm'], textStyle['text-center-xs'], textStyle[`text-${def}-md`], textStyle[`text-${def}-lg`]);
+  const colStyle = (def) =>
+    classNames(
+      textStyle['text-center-sm'],
+      textStyle['text-center-xs'],
+      textStyle[`text-${def}-md`],
+      textStyle[`text-${def}-lg`]
+    );
   return (
     <footer className={classNames(styles.footer)}>
       <Grid>
-        <Row>
+        <Row className={styles.footerRow}>
           <Col md={4} className={colStyle('left')}>
             <FooterNavigation />
           </Col>
-          <Col md={4} className={colStyle('center')}>
-            <SocialBar className={styles.social} iconClassName={styles.socialButton} />
+          <Col md={4} className={(colStyle('center'), styles.copyright)}>
             Copyright &copy; {new Date().getFullYear()} by Greg McGrath
           </Col>
           <Col md={4} className={colStyle('right')}>
-            <a
-              className={styles.btnLink}
-              href="https://github.com/mcgrathg/gwm-home"
-              target="_blank"
-            >
-              Site&apos;s Git Repo
-              <i className={classNames('fa fa-github-alt', styles.icon)} />
-            </a>
+            <SocialBar
+              className={styles.social}
+              iconClassName={styles.socialButton}
+            />
           </Col>
         </Row>
       </Grid>
