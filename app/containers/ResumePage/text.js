331b09d0395e769de99121b8c80ba44bc042d33f
Greg McGrath <greg@mcgrathg.com>
2019-11-11 20:55:16 -0500

    Add Venturi Lead React Engineer role

--- app/containers/ResumePage/text.js
+++ app/containers/ResumePage/text.js
@@ -65,42 +65,58 @@ export const skills = [
 ];
 
 export const work = [
-  {
-    title: 'Senior Software Engineer',
-    company: 'Computer Technology Services',
-    location: 'Clifton Park, NY',
-    start: 'June 2017',
+   {
+    title: 'Lead React Engineer',
+    company: 'Venturi Group',
+    location: 'Remote',
+    start: 'April 2019',
     end: 'Present',
     responsibilities: [
       {
         role: true,
         description:
-          'Leads the front-end development of Cengage’s new Progressive Web Application (PWA) using React, Redux, Webpack, and Babel to replace legacy learning management system (LMS) for 2,500 daily ESL users. To increase student engagement I prioritized accessibility, internationalization, localization, and offline usage.'
+          'Lead the front-end development of a responsive e-commerce web application using React, Redux, and TypeScript on a distributed team.'
       },
-      'Co-architected a scalable, secured API to transition from a monolithic application architecture into modern microservices; we delivered faster response rates, easier AWS deployments, improved data understanding, and support for multiple applications.',
-      'Mentored two junior developers by conducting code reviews, helping them recognize effective code, encouraging beneficial learning paths, and teaching how to solve complex problems.',
-      'Recognized and refactored common design elements into reusable, intuitive, and testable components, resulting in optimized performance, unified theme, partitioned complexity, and quicker bug-fix times.',
-      'Implemented a continuous integration and continuous deployment (CI/CD) pipeline to increase deployment frequency, as well as improve consistency and confidence across multiple build environments.',
-      'Participated in agile development process, including designing user interfaces, coding solutions, reviewing team’s code, and estimating time and effort.'
+      'Define coding standards and best practices for the front-end team, focusing on improving the coordinated efforts between two development teams.',
+      'Provide technical direction to the software development team by monitoring industry-wide web development trends.',
+      'Assess client’s needs, provide architectural recommendations, estimate required effort, participate in sprint planning, and assign tasks to engineers.',
+      'Facilitate the successful launch of new releases.',
+      'Work closely with other developers to design, develop, test, implement and support new application features, systems, and tools.'
+    ],
+  }, {
+    title: 'Senior Software Engineer',
+    company: 'Computer Technology Services',
+    location: 'Clifton Park, NY',
+    start: 'May 2017',
+    end: 'April 2019',
+    isInverted: true,
+    responsibilities: [
+      {
+        role: true,
+        description:
+          'Created a beta Progressive Web Application (PWA) using React and Redux to replace a legacy learning management system for 2,500 daily ESL users. To increase user engagement, I prioritized accessibility, internationalization, localization, and offline usage.'
+      },
+      'Co-architected a scalable, secure API to transition from a monolithic application architecture into modern microservices; we delivered faster response rates, easier AWS deployments, understandable data flows, and support for multiple applications.',
+      'Mentored two junior developers by conducting code reviews, helping them recognize effective code, encouraging beneficial learning paths, and teaching strategies for solving complex problems.',
+      'Implemented a continuous integration and continuous deployment (CI/CD) pipeline to increase deployment frequency and to improve consistency and confidence across multiple build environments.',
+      'Translated product management’s requirements into pixel-perfect, high-quality, and timely deliverables.',
+      'Participated in agile development process, including designing user interfaces, coding solutions, reviewing the team’s code, and estimating engineering effort.'
     ],
   },
   {
     title: 'Senior Web Application Developer',
     company: 'Trans World Entertainment',
     location: 'Albany, NY',
-    start: 'Dec 2016',
-    end: 'June 2017',
-    isInverted: true,
+    start: 'Nov 2016',
+    end: 'May 2017',
     responsibilities: [
       {
         role: true,
         description:
-          'Led small team by promoting functional design patterns, requiring code reviews, and mentoring. Developed and supported essential retail applications and reporting using Node.js, JavaScript, MongoDB, Docker, and React Native.',
+          'Developed and supported FYE’s essential retail store applications and reports using Node.js, JavaScript, MongoDB, Docker, and React Native.',
       },
-      'Coordinated with key stakeholders to create solutions like automated invoicing for unpaid subscriptions.',
-      'Researched new technologies, built prototypes, and evangelized successes like ECMAScript 6 and CI/CD.',
-      'Standardized ten diverse legacy projects to improve stability, reduce complexity, and increase agility.',
-      'Translated product management’s requirements into high-quality, timely deliverables.'
+      'Learned and then taught my coworker ECMAScript 2015 (ES6), Docker, and MongoDB in order to create an automated invoicing solution for unpaid magazine subscriptions.',
+      'Standardized ten diverse legacy projects to improve stability, reduce complexity, and increase agility.'
     ],
   },
   {
@@ -108,11 +124,12 @@ export const work = [
     company: "Stewart's Shops",
     location: 'Saratoga Springs, NY',
     start: 'Apr 2010',
-    end: 'Dec 2016',
+    end: 'Nov 2016',
+    isInverted: true,
     responsibilities: [
-      'Trained eight-member team in best practices for architecture design patterns and ideal usage of Ext JS, as well as proposing new engineering productivity practices and tools.',
-      'Developed and supported single-page applications providing sophisticated, tailored, and efficient solutions for 475 users.',
-      'Spearheaded creation of a shared, tested code library to reduce development time of robust interfaces.',
+      'Mentored eight engineers through quarterly presentations about front-end architecture design patterns, leading to the adoption of the Model–View–ViewModel pattern across all new projects.',
+      'Developed single-page applications providing tailored solutions that reduced costs and effort for 475 corporate users.',
+      'Created a company-wide component library to reduce the development time of feature-rich user interfaces.',
       'Streamlined challenging corporate tasks by assessing needs, architecting solutions, and developing instinctive software solutions.'
     ],
   },
@@ -120,13 +137,12 @@ export const work = [
     title: 'Full Stack Engineer',
     company: 'Independent Consulting',
     location: 'Remote',
-    start: 'Mar 2013',
-    end: 'Dec 2016',
-    isInverted: true,
+    start: 'Mar 2010',
+    end: 'June 2018',
     cardClass: styles.spacing,
     responsibilities: [
       'Completed independent, remote projects using Ext JS, PHP, and MySQL for diverse, international clients.',
-      'Developed mobile-first user client for internet startup while collaborating with distributed team.',
+      'Developed mobile-first user client for internet startup while collaborating with a distributed team.',
       'Gained strong interpersonal skills by managing communication and workflow across simultaneous projects.'
     ],
   },
